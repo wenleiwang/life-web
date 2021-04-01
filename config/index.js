e0d608c2840ea0357e2be12189d55228af77e444
wangwenlei <wangwenlei@fang.com>
2021-04-01 23:59:24 +0800

    <打包>：静态路径改动

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target: "http://localhost:2716/",
+        target: "http://39.108.141.221:2716/",
         changeOrigin:true,
         pathRewrite:{
             '^/api':''
@@ -54,7 +54,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
